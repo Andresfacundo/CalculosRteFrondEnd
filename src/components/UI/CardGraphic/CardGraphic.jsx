6d9803e5c986e513e951b3c75a626140eb20ac70
andresfacundo <andresfelipefacundo@gmail.com>
2025-02-28 16:56:48 -0500

    se crea componenente de CardGraphic

---
+++ src/components/UI/CardGraphic/CardGraphic.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CardGraphic = ({content,content2,content3,style}) => {
+  return (
+    <div className={style}>
+        <h3>{content}</h3>
+        <span>{content2}</span>
+        <span>{content3}</span>
+    </div>
+  )
+}
+
+export default CardGraphic
