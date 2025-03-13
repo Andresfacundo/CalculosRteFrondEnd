1724911928ab5eb32ceb2cd2769b7bfe0789b15a
andresfacundo <andresfelipefacundo@gmail.com>
2025-03-13 09:03:47 -0500

    se ajustan tamaños para movil en empleador y trabajador

--- src/components/Layouts/ResultDifference/ResultDiference.jsx
+++ src/components/Layouts/ResultDifference/ResultDiference.jsx
@@ -29,33 +29,36 @@ const ResultDiference = ({ selectedActual, selectProyections }) => {
       </div>
       <div className='content-difference'>
         <div className='concept'>
-
-          <p>Provisiones</p>
-          <p>Aportes</p>
-          <p>Pago Neto</p>
-          <p>Total a pagar</p>
-          <p>Costo total</p>
-        </div>
-        <div className='content-actual'>
-          <Parrafos results={selectedActual} content1="proyecciones" content2="provisionesPrestacionesSociales" />
-          <Parrafos results={selectedActual} content1="proyecciones" content2="aportesEmpleador" />
-          <Parrafos results={selectedActual} content1="proyecciones" content2="pagoNetoTrabajador" />
-          <Parrafos results={selectedActual} content1="proyecciones" content2="totalPagar" />
-          <Parrafos results={selectedActual} content1="proyecciones" content2="costoTotalEmpleador" />
-        </div>
-        <div className='content-proyectado'>
-          <Parrafos results={selectProyections} content1="proyecciones" content2="provisionesPrestacionesSociales" />
-          <Parrafos results={selectProyections} content1="proyecciones" content2="aportesEmpleador" />
-          <Parrafos results={selectProyections} content1="proyecciones" content2="pagoNetoTrabajador" />
-          <Parrafos results={selectProyections} content1="proyecciones" content2="totalPagar" />
-          <Parrafos results={selectProyections} content1="proyecciones" content2="costoTotalEmpleador" />
-        </div>
-        <div className='diferencia'>
-          <Parrafos results={calcularDiferencia("proyecciones", "provisionesPrestacionesSociales")} />
-          <Parrafos results={calcularDiferencia("proyecciones", "aportesEmpleador")} />
-          <Parrafos results={calcularDiferencia("proyecciones", "pagoNetoTrabajador")} />
-          <Parrafos results={calcularDiferencia("proyecciones", "totalPagar")} />
-          <Parrafos results={calcularDiferencia("proyecciones", "costoTotalEmpleador")} />
+          <div className='result-concept'>
+            <p>Provisiones</p>
+            <Parrafos results={selectedActual} content1="proyecciones" content2="provisionesPrestacionesSociales" />
+            <Parrafos results={selectProyections} content1="proyecciones" content2="provisionesPrestacionesSociales" />
+            <Parrafos results={calcularDiferencia("proyecciones", "provisionesPrestacionesSociales")} />
+          </div>
+          <div className='result-concept'>
+            <p>Aportes</p>
+            <Parrafos results={selectedActual} content1="proyecciones" content2="aportesEmpleador" />
+            <Parrafos results={selectProyections} content1="proyecciones" content2="aportesEmpleador" />
+            <Parrafos results={calcularDiferencia("proyecciones", "aportesEmpleador")} />
+          </div>
+          <div className='result-concept'>
+            <p>Pago Neto</p>
+            <Parrafos results={selectedActual} content1="proyecciones" content2="pagoNetoTrabajador" />
+            <Parrafos results={selectProyections} content1="proyecciones" content2="pagoNetoTrabajador" />
+            <Parrafos results={calcularDiferencia("proyecciones", "pagoNetoTrabajador")} />
+          </div>
+          <div className='result-concept'>
+            <p>Total a pagar</p>
+            <Parrafos results={selectedActual} content1="proyecciones" content2="totalPagar" />
+            <Parrafos results={selectProyections} content1="proyecciones" content2="totalPagar" />
+            <Parrafos results={calcularDiferencia("proyecciones", "totalPagar")} />
+          </div>
+          <div className='result-concept'>
+            <p>Costo total</p>
+            <Parrafos results={selectedActual} content1="proyecciones" content2="costoTotalEmpleador" />
+            <Parrafos results={selectProyections} content1="proyecciones" content2="costoTotalEmpleador" />
+            <Parrafos results={calcularDiferencia("proyecciones", "costoTotalEmpleador")} />
+          </div>
         </div>
       </div>
       <h2>Trabajador</h2>
@@ -67,35 +70,41 @@ const ResultDiference = ({ selectedActual, selectProyections }) => {
       </div>
       <div className='content-difference'>
         <div className='concept'>
-          <p>Ingresos</p>
-          <p>Aportes</p>
-          <p>ReteFuente</p>
-          <p>Deducciones</p>
-          <p>Pago neto</p>
-        </div>
-        <div className='content-actual'>
-          <Parrafos results={selectedActual} content1="calculations" content2="totalIngresos" />
-          <Parrafos results={selectedActual} content1="proyecciones" content2="aportesTrabajador" />
-          <Parrafos results={selectedActual} content1="proyecciones" content2="reteFuente" />
-          <Parrafos results={selectedActual} content1="proyecciones" content2="deducciones" />
-          <Parrafos results={selectedActual} content1="proyecciones" content2="pagoNetoTrabajador" />
-        </div>
-        <div className='content-proyectado'>
-          <Parrafos results={selectProyections} content1="calculations" content2="totalIngresos" />
-          <Parrafos results={selectProyections} content1="proyecciones" content2="aportesTrabajador" />
-          <Parrafos results={selectProyections} content1="proyecciones" content2="reteFuente" />
-          <Parrafos results={selectProyections} content1="proyecciones" content2="deducciones" />
-          <Parrafos results={selectProyections} content1="proyecciones" content2="pagoNetoTrabajador" />
-        </div>
-        <div className='diferencia'>
-          <Parrafos results={calcularDiferencia("calculations", "totalIngresos")} />
-          <Parrafos results={calcularDiferencia("proyecciones", "aportesTrabajador")} />
-          <Parrafos results={calcularDiferencia("proyecciones", "reteFuente")} />
-          <Parrafos results={calcularDiferencia("proyecciones", "deducciones")} />
-          <Parrafos results={calcularDiferencia("proyecciones", "pagoNetoTrabajador")} />          
+          <div className='result-concept'>
+            <p>Ingresos</p>
+            <Parrafos results={selectedActual} content1="calculations" content2="totalIngresos" />
+            <Parrafos results={selectProyections} content1="calculations" content2="totalIngresos" />
+            <Parrafos results={calcularDiferencia("calculations", "totalIngresos")} />
+          </div>
+          <div className='result-concept'>
+            <p>Aportes</p>
+            <Parrafos results={selectedActual} content1="proyecciones" content2="aportesTrabajador" />
+            <Parrafos results={selectProyections} content1="proyecciones" content2="aportesTrabajador" />
+            <Parrafos results={calcularDiferencia("proyecciones", "aportesTrabajador")} />
+          </div>
+          <div className='result-concept'>
+            <p>ReteFuente</p>
+            <Parrafos results={selectedActual} content1="proyecciones" content2="reteFuente" />
+            <Parrafos results={selectProyections} content1="proyecciones" content2="reteFuente" />
+            <Parrafos results={calcularDiferencia("proyecciones", "reteFuente")} />
+          </div>
+          <div className='result-concept'>
+            <p>Deducciones</p>
+            <Parrafos results={selectedActual} content1="proyecciones" content2="deducciones" />
+            <Parrafos results={selectProyections} content1="proyecciones" content2="deducciones" />
+            <Parrafos results={calcularDiferencia("proyecciones", "deducciones")} />
+          </div>
+          <div className='result-concept'>
+            <p>Pago neto</p>
+            <Parrafos results={selectedActual} content1="proyecciones" content2="pagoNetoTrabajador" />
+            <Parrafos results={selectProyections} content1="proyecciones" content2="pagoNetoTrabajador" />
+            <Parrafos results={calcularDiferencia("proyecciones", "pagoNetoTrabajador")} />
+
+          </div>
+      
         </div>
       </div>
-      
+
     </div>
   );
 };
