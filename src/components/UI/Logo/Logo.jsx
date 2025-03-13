4ce5d5abe72f1edf74d96a4d0c6963eadaf66841
andresfacundo <andresfelipefacundo@gmail.com>
2025-03-13 09:08:08 -0500

    change in logo redirect

--- src/components/UI/Logo/Logo.jsx
+++ src/components/UI/Logo/Logo.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
     <div className="container-img">
-      <img src={logo} alt="" />
+      <Link to="/">
+        <img src={logo} alt="" />
+      </Link>
     </div>
   );
 };
